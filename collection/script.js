b47ce8e02339a84e0f188604b12ba2cadbb10f2f
aliyahnc <31521442+aliyahnc@users.noreply.github.com>
2017-12-04 16:52:36 -0500

    Add files via upload

--- collection/script.js
+++ collection/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
 
+	$(".img1").hover(function() {
+		$("body").css("background-image", "url(stars.png)");
+	});
 
 });
---
+++ collection/script2.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$(".img1").hover(function() {
+		$("body").css("background-image", "url(bouquet2.jpg)");
+	});
+
+	$(".img2").hover(function() {
+		$("body").css("background-image", "url(lucky.jpg)");
+	});
+
+});
